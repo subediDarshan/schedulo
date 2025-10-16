8793af45e51c618a605412e4ddb502dd8dd9c11f
Darshan Subedi <subedidarshan02@gmail.com>
2025-10-16 23:58:10 +0530

    Add docker setup for postgres db

---
+++ pkg/db/setup.sql
@@ -0,0 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE tasks (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    endpoint TEXT NOT NULL,
+    scheduled_at TIMESTAMP NOT NULL,
+    picked_at TIMESTAMP,
+    started_at TIMESTAMP,
+    completed_at TIMESTAMP,
+    failed_at TIMESTAMP
+);
+
+CREATE INDEX idx_scheduled_at ON tasks (scheduled_at);
